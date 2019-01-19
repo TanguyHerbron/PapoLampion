1de3d07f7ce90a168bdb5f1baaf06106797b7124
Gabriel Lemée <gabigabdu72@hotmail.com>
2019-01-20 00:34:04 +0100

    interface update

--- src/main/java/com/papo/ui/controller/TestController.java
+++ src/main/java/com/papo/ui/controller/TestController.java
@@ -6,7 +6,7 @@ import java.util.HashSet;
 import java.util.ResourceBundle;
 import java.util.Set;
 
-import javafx.beans.value.ChangeListener ;
+import javafx.application.Platform;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
@@ -15,7 +15,6 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import com.papo.lib.Laumio;
 
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -25,17 +24,26 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class TestController implements Initializable {
+	@FXML private Pane Pane_Connection;
+	@FXML private Pane Pane_Functions;
     @FXML private Button Button_Send;
+    @FXML private Button Button_Exit;
+    @FXML private Button Button_Connect;
     @FXML private ColorPicker ColorPicker_Color1;
     @FXML private ChoiceBox<String> ChoiceBox_LaumioID;
     @FXML private ChoiceBox<String> ChoiceBox_Command;
     @FXML private ListView<String> ListView_Enable;
     @FXML private ListView<String> ListView_Disable;
     @FXML private ProgressBar ProgressBar_Distance;
+    @FXML private TextField TextField_IP;
+    @FXML private TextField TextField_Port;
+    
     
     private Set<String> idList;
     Color color;
@@ -44,73 +52,63 @@ public class TestController implements Initializable {
     
 	public void initialize(URL location, ResourceBundle resources) {
 		
-		//ListView_Enable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		ConnectionInit();
+	}
+	
+	public void ConnectionInit()
+	{
+		Pane_Functions.setVisible(false);
 		
-		idList = new HashSet<String>();
-		
+		Button_Connect.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				String connectStr = new String("tcp://" + TextField_IP.getText() + ":" + TextField_Port.getText());
+				System.out.println(connectStr);
+				Connect(connectStr);
+			}
+        });
+	}
+	
+	public void Connect(String ip)
+	{
 		try {
-			laumio = new Laumio("tcp://mpd.lan:1883", new MqttCallback() {
+			laumio = new Laumio(ip, new MqttCallback() {
 			    public void connectionLost(Throwable throwable) {
 			        System.out.println("Disconnected");
 			    }
 
 			    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-			        //System.out.println(">> " + s + " " + new String(mqttMessage.getPayload()));
 			        MessageHandler(s, mqttMessage);
 			    }
 
 			    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-			        //When a message is sent
+			    	try {
+						System.out.println(iMqttDeliveryToken.getMessage().getPayload());
+					} catch (MqttException e) {
+						e.printStackTrace();
+					}
 			    }
 			});
-			
-			
-	        laumio.listenTo("laumio/status/advertise");
-	        laumio.listenTo("distance/value");
-	        laumio.lookForIDs();
-	        
+			ListnerInit();
+			FunctionsInit();
 	        
 		} catch (MqttException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		ListView_Enable.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				// TODO Auto-generated method stub
-				if(oldValue != null && oldValue != "")
-				{
-					EnableItem(newValue);
-				}
-				ListView_Enable.getSelectionModel().clearSelection();
-			}
-        }); 
-		
-		ListView_Disable.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				// TODO Auto-generated method stub
+	}
 
-				if(oldValue != null && oldValue != "")
-				{
-					DisableItem(newValue);
-				}
-				ListView_Disable.getSelectionModel().clearSelection();
-			}
-        }); 
+	public void ListnerInit() throws MqttException {
+        laumio.listenTo("laumio/status/advertise");
+        laumio.listenTo("distance/value");
+        laumio.lookForIDs();
+	}
+	
+	public void FunctionsInit() {
+		Pane_Connection.setVisible(false);
+		Pane_Functions.setVisible(true);
 		
-		Button_Send.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-				// TODO Auto-generated method stub
-				color = ColorPicker_Color1.getValue();
-				try {
-					laumio.fill((int)Math.floor(color.getRed()*255), (int)Math.floor(color.getGreen()*255), (int)Math.floor(color.getBlue()*255));
-					System.out.println(color.getRed());
-				} catch (MqttException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-        });
+		//Interface init
+		InitButtons();
+		InitListView();
 	}
 	
 	public void MessageHandler(String topic, MqttMessage mqttMessage)
@@ -122,48 +120,70 @@ public class TestController implements Initializable {
             if(size < idList.size()) {
             	AddNewToListDisable(new String(mqttMessage.getPayload()));
             }
-            else
-            	{
-            	if(size > idList.size()) {
-                	RemoveInListDisable(new String(mqttMessage.getPayload()));
-                }
-            	}
         }
 
         if(topic.equals("distance/value"))
         {
-        	ProgressBar_Distance.setProgress((int)Math.floor(Double.parseDouble(new String(mqttMessage.getPayload()))));
-        	 //(int)Math.floor(Double.parseDouble(new String(mqttMessage.getPayload())));
+        	ProgressBar_Distance.setProgress(Double.parseDouble(new String(mqttMessage.getPayload())));
         }
 	}
 
-	public void AddNewToListEnable(String id) {
-		ListView_Enable.getItems().add(id);
+	public void InitButtons() {
+		
+		Button_Send.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				color = ColorPicker_Color1.getValue();
+				try {
+					laumio.fill(new HashSet<String>(ListView_Enable.getItems()), (int)Math.floor(color.getRed()*255), (int)Math.floor(color.getGreen()*255), (int)Math.floor(color.getBlue()*255));
+					//laumio.fill((int)Math.floor(color.getRed()*255), (int)Math.floor(color.getGreen()*255), (int)Math.floor(color.getBlue()*255));
+					System.out.println(color.getRed());
+				} catch (MqttException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+        });
+		
+		Button_Exit.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				try {
+					laumio.close();
+					Stage stage = (Stage) Button_Exit.getScene().getWindow();
+				    stage.close();
+				} catch (MqttException e) {
+					e.printStackTrace();
+				}
+			}
+        });
 	}
 	
-	public void RemoveInListEnable(String id)
-	{
-		ListView_Enable.getItems().remove(id);
+	public void InitListView() {
+		ListView_Disable.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> {
+		    if (newValue != null) {
+
+		        Platform.runLater(() -> {
+		        	ListView_Disable.getSelectionModel().select(-1);
+		            ListView_Enable.getItems().add(newValue);
+		            ListView_Disable.getItems().remove(newValue);
+		        });
+		    }
+		});
+		
+		ListView_Enable.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> {
+		    if (newValue != null) {
+
+		        Platform.runLater(() -> {
+		        	ListView_Enable.getSelectionModel().select(-1);
+		        	ListView_Disable.getItems().add(newValue);
+		        	ListView_Enable.getItems().remove(newValue);
+		        });
+		    }
+		});
 	}
 	
 	public void AddNewToListDisable(String id) {
 		ListView_Disable.getItems().add(id);
 	}
 	
-	public void RemoveInListDisable(String id)
-	{
-		ListView_Disable.getItems().remove(id);
-	}
-
-	public void	EnableItem(String id)
-	{
-		RemoveInListEnable(id);
-		AddNewToListDisable(id);
-	}
-	
-	public void	DisableItem(String id)
-	{
-		RemoveInListDisable(id);
-		AddNewToListEnable(id);
-	}
 }
